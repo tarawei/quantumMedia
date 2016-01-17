3304acdf6d62f31a8296f15367770609e9cb0547
Tara <tara333662002@yahoo.com.tw>
2016-01-17 19:51:20 +0800

    first time

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	$(".toggle").on("click", function(){
+		$(".nav").toggleClass("close-nav");
+	});
+	$(".nav a").on("click", function(){
+		$(".nav").addClass("close-nav");
+	});
+
+});
