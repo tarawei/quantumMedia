d80676355beb1de2bea2496bab42f77843154003
ben-hsieh <ben.hsieh@tarokosoftware.com>
2016-01-21 22:27:49 +0800

    割切 main.scss 變成 app.scss -> 然後在切成 header, content, footer content 裡面再切成 數塊 (如果還有更深那就再切)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,6 @@ jQuery(document).ready(function($) {
 	});
 	$(".nav a").on("click", function(){
 		$(".nav").addClass("close-nav");
-		$.fn.pagepiling.moveTo($(this).data('index'));
 	});
 
-	$(".logo-box a").on("click", function(){
-		$('.pages').slick('slickGoTo', $(this).data('index'));
-	})
-
-	$('#pagepiling').pagepiling({
-		direction: 'horizontal'
-	});
-
-	$.fn.pagepiling.setAllowScrolling(false);
 });
